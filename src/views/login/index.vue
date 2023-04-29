fc8c7fb07d94147414c63d58b4ea3ae3d1b94d82
leeron <leeron6@163.com>
2023-04-29 18:04:29 +0800

    '项目搭建'

---
+++ src/views/login/index.vue
@@ -0,0 +1,313 @@
+<template>
+  <div class="login-container">
+    <vue-particles
+      color="#fff"
+      :particleOpacity="0.7"
+      :particlesNumber="100"
+      shapeType="star"
+      :particleSize="6"
+      linesColor="#fff"
+      :linesWidth="1"
+      :lineLinked="true"
+      :lineOpacity="0.4"
+      :linesDistance="150"
+      :moveSpeed="3"
+      :hoverEffect="true"
+      hoverMode="grab"
+      :clickEffect="true"
+      clickMode="repulse"
+    />
+    <el-form
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-form"
+      auto-complete="on"
+      label-position="left"
+    >
+
+      <div class="title-container">
+        <img
+          class="img-class"
+          src="~@/assets/images/login-bg.png"
+          alt=""
+        >
+      </div>
+
+      <el-form-item prop="username">
+        <span class="svg-container">
+          <svg-icon icon-class="user"/>
+        </span>
+        <el-input
+          ref="username"
+          v-model="loginForm.username"
+          placeholder="账号"
+          name="username"
+          type="text"
+          tabindex="1"
+          auto-complete="on"
+        />
+      </el-form-item>
+
+      <el-form-item prop="password">
+        <span class="svg-container">
+          <svg-icon icon-class="password"/>
+        </span>
+        <el-input
+          :key="passwordType"
+          ref="password"
+          v-model="loginForm.password"
+          :type="passwordType"
+          placeholder="密码"
+          name="password"
+          tabindex="2"
+          auto-complete="on"
+          @keyup.enter.native="handleLogin"
+        />
+        <span
+          class="show-pwd"
+          @click="showPwd"
+        >
+          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"/>
+        </span>
+      </el-form-item>
+
+      <el-button
+        class="btn-class"
+        :loading="loading"
+        type="primary"
+        style="width:100%;margin-bottom:30px;"
+        @click.native.prevent="handleLogin"
+      >Login
+      </el-button>
+
+      <div class="tips">
+        <span style="margin-right:20px;">账号: admin</span>
+        <span> 密码: any</span>
+      </div>
+
+    </el-form>
+  </div>
+</template>
+
+<script>
+/*
+vue-particles的属性：
+color: String类型。默认'#dedede'。粒子颜色。
+particleOpacity: Number类型。默认0.7。粒子透明度。
+particlesNumber: Number类型。默认80。粒子数量。
+shapeType: String类型。默认'circle'。可用的粒子外观类型有：“circle”,“edge”,“triangle”, “polygon”,“star”。
+particleSize: Number类型。默认80。单个粒子大小。
+linesColor: String类型。默认'#dedede'。线条颜色。
+linesWidth: Number类型。默认1。线条宽度。
+lineLinked: 布尔类型。默认true。连接线是否可用。
+lineOpacity: Number类型。默认0.4。线条透明度。
+linesDistance: Number类型。默认150。线条距离。
+moveSpeed: Number类型。默认3。粒子运动速度。
+hoverEffect: 布尔类型。默认true。是否有hover特效。
+hoverMode: String类型。默认true。可用的hover模式有: “grab”, “repulse”, “bubble”。
+clickEffect: 布尔类型。默认true。是否有click特效。
+clickMode: String类型。默认true。可用的click模式有: “push”, “remove”, “repulse”, “bubble”
+ */
+import { validUsername } from '@/utils/validate'
+
+export default {
+  name: 'Login',
+  data() {
+    const validateUsername = (rule, value, callback) => {
+      if (!validUsername(value)) {
+        callback(new Error('请正确输入账号'))
+      } else {
+        callback()
+      }
+    }
+    const validatePassword = (rule, value, callback) => {
+      if (value.length < 6) {
+        callback(new Error('请输入任意6位数'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      loginRules: {
+        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
+        password: [{ required: true, trigger: 'blur', validator: validatePassword }]
+      },
+      loading: false,
+      passwordType: 'password',
+      redirect: undefined
+    }
+  },
+  watch: {
+    $route: {
+      handler: function(route) {
+        this.redirect = route.query && route.query.redirect
+      },
+      immediate: true
+    }
+  },
+  methods: {
+    showPwd() {
+      if (this.passwordType === 'password') {
+        this.passwordType = ''
+      } else {
+        this.passwordType = 'password'
+      }
+      this.$nextTick(() => {
+        this.$refs.password.focus()
+      })
+    },
+    handleLogin() {
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          this.loading = true
+          this.$store.dispatch('user/login', this.loginForm).then(() => {
+            this.$router.push({ path: this.redirect || '/' })
+            this.loading = false
+          }).catch(() => {
+            this.loading = false
+          })
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+/* 修复input 背景不协调 和光标变色 */
+/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
+
+$bg: #283443;
+$light_gray: #fff;
+$cursor: #fff;
+
+@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
+  .login-container .el-input input {
+    color: $cursor;
+  }
+}
+
+/* reset element-ui css */
+.login-container {
+  .el-input {
+    display: inline-block;
+    height: 47px;
+    width: 85%;
+
+    input {
+      background: transparent;
+      border: 0px;
+      -webkit-appearance: none;
+      border-radius: 0px;
+      padding: 12px 5px 12px 15px;
+      color: $light_gray;
+      height: 47px;
+      caret-color: $cursor;
+
+      &:-webkit-autofill {
+        box-shadow: 0 0 0px 1000px $bg inset !important;
+        -webkit-text-fill-color: $cursor !important;
+      }
+    }
+  }
+
+  .el-form-item {
+    //border: 1px solid rgba(255, 255, 255, 0.1);
+    //background: rgba(0, 0, 0, 0.2);
+    background: #022a3270;
+    border-radius: 5px;
+    color: #454545;
+  }
+}
+</style>
+
+<style
+  lang="scss"
+  scoped
+>
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+
+#particles-js {
+  background: url("~@/assets/images/dh.jpeg") no-repeat center center;
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: -9;
+}
+
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  overflow: hidden;
+
+  .login-form {
+    backdrop-filter: blur(5px) saturate(180%);
+    -webkit-backdrop-filter: blur(5px) saturate(180%);
+    background-color: rgba(255, 255, 255, 0.2);
+    border-radius: 12px;
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    padding: 32px;
+    margin: 300px auto;
+    overflow: hidden;
+
+    .btn-class {
+      background: #022a3280;
+      border: none;
+    }
+  }
+
+  .tips {
+    font-size: 14px;
+    color: #fff;
+    margin-bottom: 10px;
+
+    span {
+      &:first-of-type {
+        margin-right: 16px;
+      }
+    }
+  }
+
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    width: 30px;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+    text-align: center;
+    margin-bottom: 16px;
+
+    .img-class {
+      width: 125px;
+      height: 80px;
+    }
+  }
+
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+}
+</style>
